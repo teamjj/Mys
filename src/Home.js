2b6ed43f25b1ec431109070299f4bbde430d5ad1
lwj <848565635@qq.com>
2018-08-18 19:36:14 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <p className="App-intro">
+          Home
+        </p>
+      </div>
+    );
+  }
+}
+
